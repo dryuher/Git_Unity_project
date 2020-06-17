1e4932cb818360bc84cc61fe0f4061d77d4e5c72
ShatunovAndrey <dryuher8@gmail.com>
2020-06-17 20:02:11 +0500

    я добавил папку

---
+++ MySQL_database/userDelete.php
@@ -0,0 +1,15 @@
+<?php
+include('connection.php');
+
+$whereField = $_POST['whereField'];
+$whereCondition = $_POST['whereCondition'];
+
+$sql="delete from users where".$whereField."='".$whereCondition."'";
+$result - mysqli_query($connect, $sql);
+
+
+}
+}
+
+
+?>
